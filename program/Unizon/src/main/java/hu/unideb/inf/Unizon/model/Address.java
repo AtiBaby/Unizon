77b7a7e2d5ac36e331a75eecb79c337a28c22723
Czuczor Gergő <czuczorgergo@gmail.com>
2016-04-01 22:35:55 +0200

    Bugfix for address/NPE.

--- program/Unizon/src/main/java/hu/unideb/inf/Unizon/model/Address.java
+++ program/Unizon/src/main/java/hu/unideb/inf/Unizon/model/Address.java
@@ -6,209 +6,206 @@ import java.util.List;
 
 /**
  * The persistent class for the ADDRESS database table.
- * 
+ *
  */
 @Entity
 @Table(name = "ADDRESS")
 @NamedQuery(name = "Address.findAll", query = "SELECT a FROM Address a")
 public class Address implements Serializable {
-	private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "ADDRESS_ID", unique = true, nullable = false)
-	private int addressId;
+    private static final long serialVersionUID = 1L;
 
-	@Column(name = "CITY", length = 100)
-	private String city;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ADDRESS_ID", unique = true, nullable = false)
+    private int addressId;
 
-	@Column(name = "COUNTRY", length = 100)
-	private String country;
+    @Column(name = "CITY", length = 100)
+    private String city;
 
-	@Column(name = "DOOR")
-	private int door;
+    @Column(name = "COUNTRY", length = 100)
+    private String country;
 
-	@Column(name = "FLOOR")
-	private int floor;
+    @Column(name = "DOOR")
+    private Integer door;
 
-	@Column(name = "STR_NUMBER")
-	private int strNumber;
+    @Column(name = "FLOOR")
+    private Integer floor;
 
-	@Column(name = "STREET", length = 100)
-	private String street;
+    @Column(name = "STR_NUMBER")
+    private int strNumber;
 
-	@Column(name = "ZIP")
-	private String zip;
+    @Column(name = "STREET", length = 100)
+    private String street;
 
-	// bi-directional many-to-one association to AddressesOfUser
-	@OneToMany(mappedBy = "address", fetch = FetchType.EAGER)
-	private List<AddressesOfUser> addressesOfUsers;
+    @Column(name = "ZIP")
+    private String zip;
 
-	// bi-directional many-to-one association to Order
-	@OneToMany(mappedBy = "address1", fetch = FetchType.EAGER)
-	private List<Order> orders1;
+    // bi-directional many-to-one association to AddressesOfUser
+    @OneToMany(mappedBy = "address", fetch = FetchType.EAGER)
+    private List<AddressesOfUser> addressesOfUsers;
 
-	// bi-directional many-to-one association to Order
-	@OneToMany(mappedBy = "address2", fetch = FetchType.EAGER)
-	private List<Order> orders2;
+    // bi-directional many-to-one association to Order
+    @OneToMany(mappedBy = "address1", fetch = FetchType.EAGER)
+    private List<Order> orders1;
 
-	// //bi-directional many-to-one association to UserData
-	// @OneToMany(mappedBy="address", fetch=FetchType.EAGER)
-	// private List<UserData> userData;
+    // bi-directional many-to-one association to Order
+    @OneToMany(mappedBy = "address2", fetch = FetchType.EAGER)
+    private List<Order> orders2;
 
-	public Address() {
-	}
+    // //bi-directional many-to-one association to UserData
+    // @OneToMany(mappedBy="address", fetch=FetchType.EAGER)
+    // private List<UserData> userData;
+    public Address() {
+    }
 
-	public int getAddressId() {
-		return this.addressId;
-	}
+    public int getAddressId() {
+        return this.addressId;
+    }
 
-	public void setAddressId(int addressId) {
-		this.addressId = addressId;
-	}
+    public void setAddressId(int addressId) {
+        this.addressId = addressId;
+    }
 
-	public String getCity() {
-		return this.city;
-	}
+    public String getCity() {
+        return this.city;
+    }
 
-	public void setCity(String city) {
-		this.city = city;
-	}
+    public void setCity(String city) {
+        this.city = city;
+    }
 
-	public String getCountry() {
-		return this.country;
-	}
+    public String getCountry() {
+        return this.country;
+    }
 
-	public void setCountry(String country) {
-		this.country = country;
-	}
+    public void setCountry(String country) {
+        this.country = country;
+    }
 
-	public int getDoor() {
-		return this.door;
-	}
+    public Integer getDoor() {
+        return door;
+    }
 
-	public void setDoor(int door) {
-		this.door = door;
-	}
+    public void setDoor(Integer door) {
+        this.door = door;
+    }
 
-	public int getFloor() {
-		return this.floor;
-	}
+    public Integer getFloor() {
+        return floor;
+    }
 
-	public void setFloor(int floor) {
-		this.floor = floor;
-	}
+    public void setFloor(Integer floor) {
+        this.floor = floor;
+    }
 
-	public int getStrNumber() {
-		return this.strNumber;
-	}
+    public int getStrNumber() {
+        return this.strNumber;
+    }
 
-	public void setStrNumber(int strNumber) {
-		this.strNumber = strNumber;
-	}
+    public void setStrNumber(int strNumber) {
+        this.strNumber = strNumber;
+    }
 
-	public String getStreet() {
-		return this.street;
-	}
+    public String getStreet() {
+        return this.street;
+    }
 
-	public void setStreet(String street) {
-		this.street = street;
-	}
+    public void setStreet(String street) {
+        this.street = street;
+    }
 
-	public String getZip() {
-		return this.zip;
-	}
+    public String getZip() {
+        return this.zip;
+    }
 
-	public void setZip(String zip) {
-		this.zip = zip;
-	}
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
 
-	public List<AddressesOfUser> getAddressesOfUsers() {
-		return this.addressesOfUsers;
-	}
+    public List<AddressesOfUser> getAddressesOfUsers() {
+        return this.addressesOfUsers;
+    }
 
-	public void setAddressesOfUsers(List<AddressesOfUser> addressesOfUsers) {
-		this.addressesOfUsers = addressesOfUsers;
-	}
+    public void setAddressesOfUsers(List<AddressesOfUser> addressesOfUsers) {
+        this.addressesOfUsers = addressesOfUsers;
+    }
 
-	public AddressesOfUser addAddressesOfUser(AddressesOfUser addressesOfUser) {
-		getAddressesOfUsers().add(addressesOfUser);
-		addressesOfUser.setAddress(this);
+    public AddressesOfUser addAddressesOfUser(AddressesOfUser addressesOfUser) {
+        getAddressesOfUsers().add(addressesOfUser);
+        addressesOfUser.setAddress(this);
 
-		return addressesOfUser;
-	}
+        return addressesOfUser;
+    }
 
-	public AddressesOfUser removeAddressesOfUser(AddressesOfUser addressesOfUser) {
-		getAddressesOfUsers().remove(addressesOfUser);
-		addressesOfUser.setAddress(null);
+    public AddressesOfUser removeAddressesOfUser(AddressesOfUser addressesOfUser) {
+        getAddressesOfUsers().remove(addressesOfUser);
+        addressesOfUser.setAddress(null);
 
-		return addressesOfUser;
-	}
+        return addressesOfUser;
+    }
 
-	public List<Order> getOrders1() {
-		return this.orders1;
-	}
+    public List<Order> getOrders1() {
+        return this.orders1;
+    }
 
-	public void setOrders1(List<Order> orders1) {
-		this.orders1 = orders1;
-	}
+    public void setOrders1(List<Order> orders1) {
+        this.orders1 = orders1;
+    }
 
-	public Order addOrders1(Order orders1) {
-		getOrders1().add(orders1);
-		orders1.setAddress1(this);
+    public Order addOrders1(Order orders1) {
+        getOrders1().add(orders1);
+        orders1.setAddress1(this);
 
-		return orders1;
-	}
+        return orders1;
+    }
 
-	public Order removeOrders1(Order orders1) {
-		getOrders1().remove(orders1);
-		orders1.setAddress1(null);
+    public Order removeOrders1(Order orders1) {
+        getOrders1().remove(orders1);
+        orders1.setAddress1(null);
 
-		return orders1;
-	}
+        return orders1;
+    }
 
-	public List<Order> getOrders2() {
-		return this.orders2;
-	}
+    public List<Order> getOrders2() {
+        return this.orders2;
+    }
 
-	public void setOrders2(List<Order> orders2) {
-		this.orders2 = orders2;
-	}
+    public void setOrders2(List<Order> orders2) {
+        this.orders2 = orders2;
+    }
 
-	public Order addOrders2(Order orders2) {
-		getOrders2().add(orders2);
-		orders2.setAddress2(this);
+    public Order addOrders2(Order orders2) {
+        getOrders2().add(orders2);
+        orders2.setAddress2(this);
 
-		return orders2;
-	}
+        return orders2;
+    }
 
-	public Order removeOrders2(Order orders2) {
-		getOrders2().remove(orders2);
-		orders2.setAddress2(null);
+    public Order removeOrders2(Order orders2) {
+        getOrders2().remove(orders2);
+        orders2.setAddress2(null);
 
-		return orders2;
-	}
+        return orders2;
+    }
 
-	// public List<UserData> getUserData() {
-	// return this.userData;
-	// }
-	//
-	// public void setUserData(List<UserData> userData) {
-	// this.userData = userData;
-	// }
-
-	// public UserData addUserData(UserData userData) {
-	// getUserData().add(userData);
-	// userData.setAddress(this);
-	//
-	// return userData;
-	// }
-
-	// public UserData removeUserData(UserData userData) {
-	// getUserData().remove(userData);
-	// userData.setAddress(null);
-	//
-	// return userData;
-	// }
-
+    // public List<UserData> getUserData() {
+    // return this.userData;
+    // }
+    //
+    // public void setUserData(List<UserData> userData) {
+    // this.userData = userData;
+    // }
+    // public UserData addUserData(UserData userData) {
+    // getUserData().add(userData);
+    // userData.setAddress(this);
+    //
+    // return userData;
+    // }
+    // public UserData removeUserData(UserData userData) {
+    // getUserData().remove(userData);
+    // userData.setAddress(null);
+    //
+    // return userData;
+    // }
 }
