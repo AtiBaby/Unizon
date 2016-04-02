1d07e95b946486c5bf2da3422e1cac1ad3c61ffe
Adam Kiss <kisssandoradam@gmail.com>
2016-04-02 20:18:53 +0200

    Readded hashCode and equals to Address entity.

--- program/Unizon/src/main/java/hu/unideb/inf/Unizon/model/Address.java
+++ program/Unizon/src/main/java/hu/unideb/inf/Unizon/model/Address.java
@@ -189,6 +189,80 @@ public class Address implements Serializable {
         return orders2;
     }
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((country == null) ? 0 : country.hashCode());
+		result = prime * result + ((door == null) ? 0 : door.hashCode());
+		result = prime * result + ((floor == null) ? 0 : floor.hashCode());
+		result = prime * result + strNumber;
+		result = prime * result + ((street == null) ? 0 : street.hashCode());
+		result = prime * result + ((zip == null) ? 0 : zip.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Address)) {
+			return false;
+		}
+		Address other = (Address) obj;
+		if (city == null) {
+			if (other.city != null) {
+				return false;
+			}
+		} else if (!city.equals(other.city)) {
+			return false;
+		}
+		if (country == null) {
+			if (other.country != null) {
+				return false;
+			}
+		} else if (!country.equals(other.country)) {
+			return false;
+		}
+		if (door == null) {
+			if (other.door != null) {
+				return false;
+			}
+		} else if (!door.equals(other.door)) {
+			return false;
+		}
+		if (floor == null) {
+			if (other.floor != null) {
+				return false;
+			}
+		} else if (!floor.equals(other.floor)) {
+			return false;
+		}
+		if (strNumber != other.strNumber) {
+			return false;
+		}
+		if (street == null) {
+			if (other.street != null) {
+				return false;
+			}
+		} else if (!street.equals(other.street)) {
+			return false;
+		}
+		if (zip == null) {
+			if (other.zip != null) {
+				return false;
+			}
+		} else if (!zip.equals(other.zip)) {
+			return false;
+		}
+		return true;
+	}
+
     // public List<UserData> getUserData() {
     // return this.userData;
     // }
