69202fb642f17a19c0eae8be76d12d391c51ce95
Adam Kiss <kisssandoradam@gmail.com>
2016-04-03 00:11:31 +0200

    Fixed the previous reg cleanup, because I forgot to add this again.

--- program/Unizon/src/main/java/hu/unideb/inf/Unizon/facade/AddressFacade.java
+++ program/Unizon/src/main/java/hu/unideb/inf/Unizon/facade/AddressFacade.java
@@ -9,13 +9,20 @@ import java.util.Collections;
 import java.util.List;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import org.slf4j.Logger;
 
 import hu.unideb.inf.Unizon.model.Address;
 
 @Stateless
 public class AddressFacade extends AbstractFacade<Address> {
+
+	@Inject
+	private Logger log;
 
 	@PersistenceContext(unitName = "primary")
 	private EntityManager em;
@@ -39,8 +46,20 @@ public class AddressFacade extends AbstractFacade<Address> {
 		List<Address> resultList;
 
 		try {
-			resultList = em.createNamedQuery("Address.findByAllAttributes", Address.class).getResultList();
+			TypedQuery<Address> query = em.createNamedQuery("Address.findByAllAttributes", Address.class);
+
+			query.setParameter("zip", zip);
+			query.setParameter("country", country);
+			query.setParameter("country", country);
+			query.setParameter("city", city);
+			query.setParameter("street", street);
+			query.setParameter("strNumber", strNumber);
+			query.setParameter("floor", floor);
+			query.setParameter("door", door);
+
+			resultList = query.getResultList();
 		} catch (Exception e) {
+			log.error(e.getMessage());
 			resultList = Collections.emptyList();
 		}
 
