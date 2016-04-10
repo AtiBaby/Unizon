935f1be91fe1e25242588456deb7238e721e9cb1
konkolyagi <agigi1992@gmail.com>
2016-04-10 14:08:56 +0200

    And change again

--- program/Unizon/src/main/java/hu/unideb/inf/Unizon/validator/RequiredCheckboxValidator.java
+++ program/Unizon/src/main/java/hu/unideb/inf/Unizon/validator/RequiredCheckboxValidator.java
@@ -11,11 +11,15 @@ import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIInput;
 import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 
+@FacesValidator()
 public class RequiredCheckboxValidator implements Validator {
+    
 
+    @Override
     public void validate(FacesContext context, UIComponent component, Object value)
         throws ValidatorException
     {
